f203072d09c319e1e5ace106d502196fb424d6e6
Ivan Mutsko <imucko1996@gmail.com>
2023-03-12 14:16:50 +0200

    general task complite

---
+++ src/index.js
@@ -0,0 +1,102 @@
+import Notiflix from 'notiflix';
+
+import { fetchPhotos } from './js/fetchPhotos';
+
+const refs = {
+  searchInput: document.querySelector('[name="searchQuery"]'),
+  searchBtn: document.querySelector('[type="submit"]'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+};
+
+let page = 1;
+let cardsOnPage = 0;
+
+const renderMarkup = function (photos) {
+  let markup = photos
+    .map(photo => {
+      return `<div class="photo-card">
+        <img
+          src="${photo.webformatURL}"
+          alt="${photo.tags}"
+          loading="lazy"
+        />
+        <div class="info">
+          <p class="info-item likes">
+            <b>Likes</b>
+            ${photo.likes}
+          </p>
+          <p class="info-item views">
+            <b>Views</b>
+            ${photo.views}
+          </p>
+          <p class="info-item comments">
+            <b>Comments</b>
+            ${photo.comments}
+          </p>
+          <p class="info-item downloads">
+            <b>Downloads</b>
+            ${photo.downloads}
+          </p>
+        </div>
+      </div>`;
+    })
+    .join('');
+
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+};
+
+const clearGallery = () => {
+  refs.gallery.innerHTML = '';
+};
+
+const loadGalleryItems = function (evt) {
+  evt.preventDefault();
+
+  let searchQuery = refs.searchInput.value;
+
+  page = 1;
+
+  if (page === 1) {
+    clearGallery();
+  }
+
+  fetchPhotos(searchQuery, page)
+    .then(photos => {
+      if (photos.hits.length === 0) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        renderMarkup(photos.hits);
+        refs.loadMoreBtn.classList.remove('visually-hidden');
+
+        cardsOnPage = document.querySelectorAll('.photo-card').length;
+      }
+    })
+    .catch(error => console.log(error));
+};
+
+const loadMoreGalleryItems = function () {
+  page += 1;
+
+  let searchQuery = refs.searchInput.value;
+
+  cardsOnPage += document.querySelectorAll('.photo-card').length;
+
+  fetchPhotos(searchQuery, page)
+    .then(photos => {
+      if (cardsOnPage < photos.totalHits) {
+        renderMarkup(photos.hits);
+      } else {
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+        refs.loadMoreBtn.classList.add('visually-hidden');
+      }
+    })
+    .catch(error => console.log(error));
+};
+
+refs.searchBtn.addEventListener('click', loadGalleryItems);
+refs.loadMoreBtn.addEventListener('click', loadMoreGalleryItems);
